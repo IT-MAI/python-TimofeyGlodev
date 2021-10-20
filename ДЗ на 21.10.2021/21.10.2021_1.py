57e3d96ed5105ac34ca6c0351252c3838d01aa52
TimofeyGlodev <timmon2003@yandex.ru>
2021-10-20 19:48:50 +0300

    Часть заданий на 21.10.2021 от 14.09.2021

--- ДЗ на 21.10.2021/21.10.2021_1.py
+++ ДЗ на 21.10.2021/21.10.2021_1.py
@@ -1,8 +1,7 @@
-import math
 a = int(input("Первый коэфициент:"))
 b = int(input("Второй коэфициент:"))
 c = int(input("Третий коэфициент:"))
-D = b**2 - 4 * a * c
+D = (b**2) - (4 * a * c)
 print(D)
 if D > 0:
     print("Уравнение умеет 2 корня")
